29cc4a9860141da507859eff1e3194c777e1ac2a
Alex Tomkins <tomkins@darkzone.net>
2019-02-09 17:24:20 +0000

    Use updated static tag for Django 2.1

--- wagtailfontawesome/wagtail_hooks.py
+++ wagtailfontawesome/wagtail_hooks.py
@@ -1,6 +1,10 @@
-from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.utils.html import format_html
 from pkg_resources import parse_version
+
+try:
+    from django.templatetags.static import static
+except ImportError:  # fallback for Django <2.1
+    from django.contrib.staticfiles.templatetags.staticfiles import static
 
 try:
     from wagtail import __version__ as WAGTAIL_VERSION
