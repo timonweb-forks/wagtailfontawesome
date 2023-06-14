b73fb38c2d37363a4cd9312b28289896a8b2df91
Tim Kamanin <timur@kamanin.com>
2023-06-14 11:26:11 +0200

    Adds Wagtail 5.0 Support

--- wagtailfontawesome/wagtail_hooks.py
+++ wagtailfontawesome/wagtail_hooks.py
@@ -8,10 +8,10 @@ except ImportError:  # fallback for Django <2.1
 
 try:
     from wagtail import __version__ as WAGTAIL_VERSION
+    from wagtail import hooks
+except ImportError:  # fallback for Wagtail <4.2
     from wagtail.core import hooks
-except ImportError:  # fallback for Wagtail <2.0
-    from wagtail.wagtailcore import hooks
-    from wagtail.wagtailcore import __version__ as WAGTAIL_VERSION
+    from wagtaill.core import __version__ as WAGTAIL_VERSION
 
 
 def import_wagtailfontawesome_stylesheet():
